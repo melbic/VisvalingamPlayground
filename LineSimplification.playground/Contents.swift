02e16f351685619db80b0472b712e274e09274a9
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2016-10-24 08:14:14 +0200

    Refactor to class

--- LineSimplification.playground/Contents.swift
+++ LineSimplification.playground/Contents.swift
@@ -39,19 +39,25 @@ class MapViewDelegate:NSObject, MKMapViewDelegate {
     }
 }
 
-
-let frame = CGRect( x:0, y:0, width:  400, height:600)
-
-let mapView = MKMapView(frame:frame)
-
-PlaygroundPage.current.liveView = mapView
-let center = CLLocationCoordinate2D(latitude:47.368336381620345, longitude:8.518940246409496)
-
-let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+func setupMap() -> (MKMapView, MKMapViewDelegate) {
+    let frame = CGRect( x:0, y:0, width:  400, height:600)
     
-mapView.setRegion(region, animated: false)
-let delegate =  MapViewDelegate()
-mapView.delegate = delegate
+    let mapView = MKMapView(frame:frame)
+    
+    PlaygroundPage.current.liveView = mapView
+    let center = CLLocationCoordinate2D(latitude:47.368336381620345, longitude:8.518940246409496)
+    
+    let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+    
+    mapView.setRegion(region, animated: false)
+
+    let delegate = MapViewDelegate()
+    mapView.delegate = delegate
+
+    return (mapView, delegate)
+}
+
+let (mapView, mapViewDelegate) = setupMap()
 
 let input = [[8.518940246409496,47.368336381620345],[8.518714940852211,47.36869970309936],[8.519476688212407,47.3688668301392],[8.518854415720982,47.36963706095784],[8.51865056783587,47.36992771117395],[8.518446719950756,47.370240158369846],[8.518446719950756,47.37037094966961],[8.51755622655797,47.370603466734806],[8.51960543424509,47.3717442387348],[8.518918788737322,47.372318248434794],[8.51960543424509,47.37273966930006],[8.521150386637473,47.37149719466227],[8.522266185587458,47.37198401569832],[8.522867000406709,47.372231057490424],[8.523414171045705,47.3723981733403],[8.523854053324008,47.372441768692376],[8.52587107450296,47.372834125239194],[8.525849616830875,47.372957644288476],[8.525956905191476,47.3730157708003],[8.526310956781355,47.373902192160806],[8.527694976632885,47.37482492643777],[8.52704051763329,47.37539890260625]].map{($0[0],$0[1])}
 var coordinates = input.map { CLLocationCoordinate2D(latitude: $0.1, longitude:$0.0)}
@@ -59,72 +65,10 @@ var coordinates = input.map { CLLocationCoordinate2D(latitude: $0.1, longitude:$
 let path = MKPolyline(coordinates:&coordinates, count: coordinates.count)
 mapView.add(path, level: .aboveRoads)
 
-typealias CoordinateTripple = (CLLocation?, CLLocation?,CLLocation?)
 
-let appendCoordinates: ([CoordinateTripple], CLLocation) -> [CoordinateTripple] = { r, cNew in
-    var result = r
-    let lastTriangle = r.last
-    let newTuple:CoordinateTripple = (lastTriangle?.1, lastTriangle?.2, c2:cNew)
-    result.append(newTuple)
-    return result}
+var simplificator = LineSimplificator(coordinates)
 
-
-let removeMin: (Double) -> (([Triangle], Triangle)->[Triangle]) =  { minimum in
-    var newC0 :CLLocation?
-
-    return { r, t in
-            var rr = r
-    if  t.area! > minimum {
-        if let c0 = newC0 {
-            var tt = t
-            tt.c0 = c0
-            newC0 = nil
-            rr.append(tt)
-        } else {
-            rr.append(t)
-        }
-    } else {
-        if var l = rr.last {
-            l.c2 = t.c2
-        }
-        newC0 = t.c0
-    }
-    return rr
-    }
-}
-
-var triangles:[Triangle] = coordinates
-    .map{ CLLocation(latitude:$0.latitude, longitude:$0.longitude)}
-    .reduce([(nil,nil,nil)], appendCoordinates)
-    .map { tuple in
-        guard let c0 = tuple.0, let c1 = tuple.1, let c2 = tuple.2
-            else { return nil }
-        return Triangle(c0: c0, c1: c1, c2: c2) }
-    .flatMap { $0 }
-
-
-for i in 0...6 {
-   triangles = triangles.map {
-        var t = $0
-        t.calcArea()
-        return t
-        }
-    let minArea = triangles.flatMap{$0.area}.reduce(Double.infinity, min)
-    print("Smallest area is:\(minArea)")
-    triangles = triangles.reduce([],removeMin(minArea))
-}
-
-enum NoElementsError: Error {
-    case NoElements
-}
-var reducedCoordinates = triangles.map { $0.c0.coordinate }
-guard let last = triangles.last else {throw  NoElementsError.NoElements }
-
-reducedCoordinates.append(last.c1.coordinate)
-reducedCoordinates.append(last.c2.coordinate)
-
-reducedCoordinates.count
-
+var reducedCoordinates = simplificator.simplify(tolerance:1000.0)
 //let polygons:[MKPolygon] = triangles.flatMap{
 //    if let a = $0.c0?.coordinate, let b = $0.c1?.coordinate, let c = $0.c2?.coordinate {
 //        var t = [a,b, c]
@@ -139,4 +83,6 @@ let reducedPath = MKPolyline(coordinates:&reducedCoordinates, count: reducedCoor
 reducedPath.title = "Reduced"
 
 mapView.add(reducedPath, level: .aboveRoads)
+
+
 
--- LineSimplification.playground/Sources/Triangle.swift
+++ LineSimplification.playground/Sources/Triangle.swift
@@ -21,6 +21,12 @@ extension Triangle {
         calcArea()
     }
     
+    public init(other: Triangle, c0: CLLocation) {
+        self.init(c0: c0, c1: other.c1, c2: other.c2)
+    }
+    public init(other: Triangle, c2: CLLocation) {
+        self.init(c0: other.c0, c1: other.c1, c2: c2)
+    }
     mutating public func calcArea()  {
         let a = c0.distance(from: c1)
         let b = c1.distance(from: c2)
@@ -28,5 +34,76 @@ extension Triangle {
         let p = (a+b+c)/2
         
         self.area = sqrt(p*(p-a) * (p-b) * (p-c))
+    }
+}
+
+typealias CoordinateTripple = (CLLocation?, CLLocation?,CLLocation?)
+
+let createTriangles: ([CoordinateTripple], CLLocation) -> [CoordinateTripple] = { r, cNew in
+    var result = r
+    let lastTriangle = r.last
+    let newTuple:CoordinateTripple = (lastTriangle?.1, lastTriangle?.2, c2:cNew)
+    result.append(newTuple)
+    return result
+}
+
+let removeMin: (Double) -> (([Triangle], Triangle)->[Triangle]) =  { min in
+    var newC0 :CLLocation?
+    
+    return { r, triangleIn in
+        var triangles = r
+        if  triangleIn.area! > min {
+            if let c0 = newC0 {
+                var newTriangle = Triangle(other: triangleIn, c0:c0)
+                newC0 = nil
+                triangles.append(newTriangle)
+            } else {
+                triangles.append(triangleIn)
+            }
+        } else {
+            if let newTriangle = triangles.last {
+                triangles[triangles.count-1] = Triangle(other:newTriangle, c2:triangleIn.c2)
+            }
+            newC0 = triangleIn.c0
+        }
+        return triangles
+    }
+}
+
+enum NoElementsError: Error {
+    case NoElements
+}
+
+public struct LineSimplificator {
+    private var triangles: [Triangle]
+    public init(_ coordinates:[CLLocationCoordinate2D]) {
+        triangles = coordinates
+            .map{ CLLocation(latitude:$0.latitude, longitude:$0.longitude)}
+            .reduce([], createTriangles)
+            .map { tuple in
+                guard let c0 = tuple.0, let c1 = tuple.1, let c2 = tuple.2
+                    else { return nil }
+                return Triangle(c0: c0, c1: c1, c2: c2) }
+            .flatMap { $0 }
+    }
+    
+    public mutating func simplify(tolerance: Double) -> [CLLocationCoordinate2D] {
+        var minArea = -1.0
+        while (minArea < tolerance ){
+           minArea = triangles.flatMap{$0.area}
+                .reduce(Double.infinity, min)
+            print("minArea: \(minArea)")
+            guard minArea < tolerance else { break }
+            triangles = triangles.reduce([],removeMin(minArea))
+           
+        }
+        
+        var reducedCoordinates = triangles.map { $0.c0.coordinate }
+        if let last = triangles.last {
+            reducedCoordinates.append(last.c1.coordinate)
+            reducedCoordinates.append(last.c2.coordinate)
+        }
+    
+        return reducedCoordinates
     }
 }
